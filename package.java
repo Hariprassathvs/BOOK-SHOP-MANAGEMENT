e545e2ed2a68d411798cc81c6997d9fe175833f6
hari prassath <68736095+Hariprassathvs@users.noreply.github.com>
2020-11-21 11:59:29 +0530

    Create package.java

---
+++ package.java
@@ -0,0 +1,16 @@
+package mypack;
+public class location
+{
+	int venueid[]=new int[2];
+public void getbookid()
+	{
+		if(bookid[0]==1)
+		{
+			System.out.println("Sherlock Holmes");
+		}
+		else
+		{
+			System.out.println("James Bond");
+		}
+}
+}
